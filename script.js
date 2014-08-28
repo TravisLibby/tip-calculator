cd1aa340f007932ed39fbe4c2a29580c843d3a94
Travis Libby <travislibby@gmail.com>
2014-08-27 20:04:23 -0700

    Create alert with bill details.

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ var baseCost;
 var taxPercentage;
 var tipPercentage;
 
-var parsePromptInput = function(formInput, isPercent) {
+function parsePromptInput(formInput, isPercent) {
   formInput = parseFloat(formInput);
-  formInput = formInput.toFixed(2);
   if (isPercent) {
     formInput /= 100;
   }
   formInput = parseFloat(formInput);
-  formInput = formInput.toFixed(2);
-  return Number(formInput);
-};
+  return formInput;
+}
 
 while (typeof baseCost === 'undefined' || baseCost === '' || isNaN(baseCost)) {
   baseCost = prompt('Please enter the base cost of your meal.');
@@ -28,15 +26,13 @@ while (typeof tipPercentage === 'undefined' || tipPercentage === '' || isNaN(tip
   tipPercentage = parsePromptInput(tipPercentage, true);
 }
 
-var baseCostPlusTax = baseCost + (baseCost * taxPercentage);
-var tipAmt = baseCostPlusTax * tipPercentage;
-var totalCost = baseCostPlusTax + tipAmt;
-console.log('Tax: ' + (baseCost * taxPercentage).toFixed(2));
-console.log(baseCostPlusTax.toFixed(2));
-console.log(tipAmt.toFixed(2));
-console.log(totalCost.toFixed(2));
+var tipAmt = baseCost * tipPercentage;
+var taxAmt = baseCost * taxPercentage;
+var totalCost = baseCost + taxAmt + tipAmt;
 
-alert('Base Cost with Tax: ' + baseCostPlusTax.toFixed(2) + '\n' + 'Tip Amount: ' + tipAmt.toFixed(2) + '\n' + 'Total Cost: ' + totalCost.toFixed(2));
+var bill = 'Base: ' + baseCost.toFixed(2) + '\n' + 'Tax: ' + taxAmt.toFixed(2) + '\n' + 'Tip: ' + tipAmt.toFixed(2) + '\n' + 'Total Cost: ' + totalCost.toFixed(2);
+
+alert(bill);
 
 
 
